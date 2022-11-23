4b5481640e91cba95a8976f6aaafb34890401441
Elianna Tirado <eliannatirado02@gmail.com>
2022-11-23 14:59:14 -0500

    Created Sequelize models

---
+++ server/db/index.js
@@ -0,0 +1,53 @@
+//POSTGRES SERVER - BACKEND
+const Sequelize = require("sequelize");
+
+//using Sequelize to connect to the database
+const dbConnection = new Sequelize("postgres://localhost:5432/moviewatchlist");
+
+// //TESTING DATABASE CONNECTION
+// const test = async () => {
+// 	try {
+// 		await dbConnection.authenticate;
+// 		console.log("authentication worked");
+// 	} catch (e) {
+// 		console.error(e);
+// 	}
+// };
+// test();
+
+//MODELS (tables) FOR DATABASE
+const Movie = dbConnection.define("movie", {
+	//COLUMNS
+	title: {
+		type: Sequelize.STRING(255),
+		allowNull: false,
+		validate: {
+			notEmpty: true, //making sure you can't add empty string (diff than null)
+		},
+	},
+	imdbLink: {
+		type: Sequelize.STRING(1000),
+		allowNull: true, //true is the default
+		validate: {
+			isUrl: true,
+		},
+	},
+	watched: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false,
+	},
+});
+
+const Genre = dbConnection.define("genre", {
+	name: {
+		type: Sequelize.STRING(50),
+		allowNull: false,
+	},
+});
+
+Movie.belongsToMany(Genre, {through: 'movies_genres'});
+Genre.belongsToMany(Movie, {through: 'movies_genres'}); 
+
+//EXPORT THE CONNECTION
+module.exports = dbConnection;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
+//MAIN SERVER FILE - API METHODS
 const express = require("express");
 const app = express();
 
 const PORT = 8080;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}!`);
-});
+
+const dbConnection = require("./db/index.js"); //do not need full path bc Node automatically looks for file named index
+
+const startServer = async () => {
+	//MAKING SURE MODELS MATCH THE DATABASE
+	await dbConnection.sync({ force: true });
+	//STARTING SERVER
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}!`);
+	});
+};
+startServer();
 
 app.get("/", (req, res) => {
 	res.send("Hello :)");
-}); //just to make sure everything is running correctly 
+}); //just to make sure everything is running correctly
